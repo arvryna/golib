47b67d5c855f62bea7f6cc7b4b62657d39266d14
arvryna <arv.pyrna@gmail.com>
2022-04-05 11:56:36 +0300

    Add GenerateAccessToken API

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # golib
-contains packages that i use in my go projects
+Collections of various modules that i use in my go projects, This lib is not ready for protection, tests were not written and in beta stages.
+
+# List of Libraries:
+- Gcalman[pkg/cal.go]: A Wrapper over Google calendar SDK, Google calendar API is too much verbose for beginners, intention was to simplify it by writing a layer above it to meet simple use cases.
--- pkg/gcalman/cal.go
+++ pkg/gcalman/cal.go
@@ -10,7 +10,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"math/rand"
 	"os"
 	"time"
 
@@ -22,8 +21,8 @@ import (
 
 type GcalMan interface {
 	GetEvents(calId string)
+	BuildCalEventObject(calEvent *GcalEvent) *calendar.Event
 	CreateEvent(calId string, event *calendar.Event) error
-	CreateCalEvent(calEvent *GcalEvent) *calendar.Event
 }
 
 type gcalman struct {
@@ -32,7 +31,54 @@ type gcalman struct {
 	CalServ     *calendar.Service
 }
 
-// File path of oauthtoken and accesstoken
+type GcalEvent struct {
+	Title          string
+	Description    string
+	AttendeeEmails []string
+	Start          string
+	End            string
+	Location       string
+	// This sends adds an entry in the users's calendar and also sends email to the user's Inbox
+	SendInvite bool
+	// By setting this flag, the event will appear in the calender of user as "AcceptedEvent"
+	AcceptedEvent bool
+}
+
+// Oauth token must be ready in advance, it can be fetched from
+// Google developer console, more instructions in the docs
+// This function Generates Auth URL in STDOUT with instructions
+// Once AccessToken is provided as STDIN, it will be stored in the preferredAccessTokenPath
+// Once the tokens are saved to disk, we can Initialize this library using Init function
+func GenerateAccessTokenFromAuthToken(authTokenPath string, preferredAccessTokenPath string) {
+	b, err := ioutil.ReadFile(authTokenPath)
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, calendar.CalendarScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	saveToken(preferredAccessTokenPath, tok)
+}
+
+// Params: File path of oauthtoken and accesstoken
+// Details about fetching the auth/access token is provided in the documentation
 func Init(oauthToken string, accessToken string) GcalMan {
 	ctx := context.Background()
 	b, err := ioutil.ReadFile(oauthToken)
@@ -89,6 +135,48 @@ func (g *gcalman) GetEvents(calId string) {
 	}
 }
 
+// calendar.Event struct is very complex with lot of features, it was abstracted
+// away by our GcalEvent struct, which is simple, After building this calendar.Event,
+// it is possible to call the CreateEvent function.
+// Create Event Object in GoogelCalendar format.
+// Example Time format "2022-03-31T10:45:26.371Z"
+func (g *gcalman) BuildCalEventObject(calEvent *GcalEvent) *calendar.Event {
+	startTime := &calendar.EventDateTime{
+		DateTime: calEvent.Start,
+	}
+
+	endTime := &calendar.EventDateTime{
+		DateTime: calEvent.End,
+	}
+
+	var attendees []*calendar.EventAttendee
+
+	var eventStatus = ""
+	if calEvent.AcceptedEvent {
+		eventStatus = "accepted"
+	}
+
+	for _, val := range calEvent.AttendeeEmails {
+		attendee := &calendar.EventAttendee{
+			Email:          val,
+			ResponseStatus: eventStatus,
+		}
+		attendees = append(attendees, attendee)
+	}
+	event := &calendar.Event{
+		Attendees:   attendees,
+		Summary:     calEvent.Title,
+		Description: calEvent.Description,
+		Start:       startTime,
+		End:         endTime,
+		Location:    calEvent.Location,
+		Reminders: &calendar.EventReminders{
+			UseDefault: calEvent.SendInvite,
+		},
+	}
+	return event
+}
+
 // Create event in Google Calender, required to pass
 func (g *gcalman) CreateEvent(calId string, event *calendar.Event) error {
 
@@ -109,13 +197,6 @@ func (g *gcalman) CreateEvent(calId string, event *calendar.Event) error {
 	return err
 }
 
-func randomString(length int) string {
-	rand.Seed(time.Now().UnixNano())
-	b := make([]byte, length)
-	rand.Read(b)
-	return fmt.Sprintf("%x", b)[:length]
-}
-
 // Retrieves a token from a local file.
 func tokenFromFile(file string) (*oauth2.Token, error) {
 	f, err := os.Open(file)
@@ -128,24 +209,6 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 	return tok, err
 }
 
-// Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
-}
-
 // Saves a token to a file path.
 func saveToken(path string, token *oauth2.Token) {
 	fmt.Printf("Saving credential file to: %s\n", path)
@@ -155,47 +218,4 @@ func saveToken(path string, token *oauth2.Token) {
 	}
 	defer f.Close()
 	json.NewEncoder(f).Encode(token)
-}
-
-type GcalEvent struct {
-	Title          string
-	Description    string
-	AttendeeEmails []string
-	Start          string
-	End            string
-	Location       string
-	SendRemainders bool
-}
-
-// Create Event Object in GoogelCalendar format.
-// Example Time format "2022-03-31T10:45:26.371Z"
-func (g *gcalman) CreateCalEvent(calEvent *GcalEvent) *calendar.Event {
-
-	startTime := &calendar.EventDateTime{
-		DateTime: calEvent.Start,
-	}
-
-	endTime := &calendar.EventDateTime{
-		DateTime: calEvent.End,
-	}
-
-	var attendees []*calendar.EventAttendee
-
-	for _, val := range calEvent.AttendeeEmails {
-		attendee := &calendar.EventAttendee{
-			Email: val,
-		}
-		attendees = append(attendees, attendee)
-	}
-	event := &calendar.Event{
-		Attendees:   attendees,
-		Summary:     calEvent.Title,
-		Description: calEvent.Description,
-		Start:       startTime,
-		End:         endTime,
-		Reminders: &calendar.EventReminders{
-			UseDefault: calEvent.SendRemainders,
-		},
-	}
-	return event
 }
