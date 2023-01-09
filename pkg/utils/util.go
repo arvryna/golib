754d0239d1d6d1769872b559a8b7b2bad38be4f2
arvryna <arv.ryna@gmail.com>
2023-01-09 22:07:25 +0300

    Add new utils

--- pkg/utils/util.go
+++ pkg/utils/util.go
@@ -3,8 +3,12 @@ package utils
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 )
 
 func GetUserInput(prompt string) string {
@@ -12,4 +16,23 @@ func GetUserInput(prompt string) string {
 	fmt.Print(prompt)
 	data, _ := reader.ReadString('\n')
 	return strings.TrimSpace(data)
+}
+
+func ToFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func Date(year, month, day int) time.Time {
+	return time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
+}
+
+func Wait() {
+	var sig = make(chan os.Signal, 2)
+	signal.Notify(sig, os.Interrupt, syscall.SIGQUIT, syscall.SIGTERM)
+	<-sig
 }
